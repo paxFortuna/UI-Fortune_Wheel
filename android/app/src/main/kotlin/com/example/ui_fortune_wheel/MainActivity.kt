d43aa1265d7a14259e86d9dd24c604f4d968b07f
fortuna <avidia38@gmail.com>
2022-10-22 19:16:05 +0900

    first: fortune wheel

---
+++ android/app/src/main/kotlin/com/example/ui_fortune_wheel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_fortune_wheel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bar_page.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+
+class BarPage extends StatefulWidget {
+  const BarPage({Key? key}) : super(key: key);
+
+  @override
+  State<BarPage> createState() => _BarPageState();
+}
+
+class _BarPageState extends State<BarPage> {
+  StreamController<int> selected1 = StreamController<int>();
+
+  @override
+  void dispose() {
+    selected1.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <String>[
+      'Grogu',
+      'Mace Windu',
+      'Obi-Wan Kenobi',
+      'Han Solo',
+      'Luke Skywalker',
+      'Darth Vader',
+      'Yoda',
+      'Ahsoka Tano',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Fortune Wheel'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 50, bottom: 50),
+            child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    selected1.add(
+                      Fortune.randomInt(0, items.length),
+                    );
+                  });
+                },
+                child: Text('Roll')),
+          ),
+          Expanded(
+            child: FortuneBar(
+              selected: selected1.stream,
+              items: const [
+                FortuneItem(child: Text('Han Solo')),
+                FortuneItem(child: Text('Yoda')),
+                FortuneItem(child: Text('Obi-Wan Kenobi')),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
